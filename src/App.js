7e612eb9c031fbe391763a53cca35c47e007b044
Muslim Hussaini <hussainimuslim2@gmail.com>
2025-04-13 16:12:46 -0400

    added creditamt and debitamt states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-/*==================================================
-src/App.js
-
-This is the top-level component of the app.
-It contains the top-level state.
-Expand
-message.js
-4 KB
-﻿
-```js
-/*==================================================
-src/App.js
-
-This is the top-level component of the app.
-It contains the top-level state.
-==================================================*/
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
@@ -31,6 +15,8 @@ class App extends Component {
       accountBalance: 0,
       credit: [],
       debit: [],
+      creditAmount: 0,
+      debitAmount: 0,
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '11/22/99',
@@ -81,6 +67,8 @@ class App extends Component {
       this.setState({
         credit: creditData,
         debit: debitData,
+        creditAmount: totalCredits,
+        debitAmount: totalDebits,
         accountBalance: balance
       });
     })
@@ -120,8 +108,8 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     )
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.credit} addCredit={this.addCredit}/>) 
-    const DebitsComponent = () => (<Debits debits={this.state.debit} addDebit={this.addDebit}/>) 
+    const CreditsComponent = () => (<Credits credits={this.state.credit} addCredit={this.addCredit} creditAmount = {this.state.creditAmount}/>) 
+    const DebitsComponent = () => (<Debits debits={this.state.debit} addDebit={this.addDebit} debitAmount = {this.state.debitAmount} />) 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
       <Router basename="/bankofreact">
